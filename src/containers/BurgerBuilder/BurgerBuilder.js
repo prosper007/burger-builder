0be95b861dda3c40fbbe991f06d01a952d490193
prosper007 <onungwap@gmail.com>
2020-06-09 17:45:47 -0400

    custom buttons

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -25,7 +25,7 @@ class BurgerBuilder extends Component {
     purchasing: false
   }
 
-  updatePurchaseState(ingredients) {
+  updatePurchaseState = (ingredients) => {
     const sum = Object.values(ingredients).reduce((sum, currEl) => {
       return sum + currEl
     }, 0);
@@ -71,6 +71,10 @@ class BurgerBuilder extends Component {
     this.setState({ purchasing: false });
   }
 
+  purchaseContinueHandler = () => {
+    alert('You continued');
+  }
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -81,7 +85,12 @@ class BurgerBuilder extends Component {
     return (
       <Aux>
         <Modal show={this.state.purchasing} closeModal={this.purchaseCancelHandler}>
-          <OrderSummary ingredients={this.state.ingredients} />
+          <OrderSummary
+            ingredients={this.state.ingredients}
+            price={this.state.totalPrice}
+            purchaseCancelled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler}
+          />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
