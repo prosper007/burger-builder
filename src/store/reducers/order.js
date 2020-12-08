da02b65223ef6a5f80615c7be0a36cc49d10e769
prosper007 <onungwap@gmail.com>
2020-12-07 22:33:42 -0500

    order reducer and actions

---
+++ src/store/reducers/order.js
@@ -0,0 +1,36 @@
+import { stat } from 'fs-extra';
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+  orders: [],
+  loading: false
+};
+
+const orderReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.PURCHASE_BURGER_START:
+      return {
+        ...state,
+        loading: true,
+      };
+    case actionTypes.PURCHASE_BURGER_SUCCESS:
+      const newOrder = {
+        ...action.orderData,
+        id: action.orderId
+      };
+      return {
+        ...state,
+        loading: false,
+        order: state.orders.concat(newOrder)
+      };
+    case actionTypes.PURCHASE_BURGER_FAILED:
+      return {
+        ...state,
+        loading: false
+      };
+    default:
+      return state;
+  }
+};
+
+export default orderReducer;
