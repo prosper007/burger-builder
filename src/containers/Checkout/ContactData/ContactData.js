22355e0c598d5b3de3e51768e2c1370482c6ee30
prosper007 <onungwap@gmail.com>
2020-12-12 03:08:25 -0500

    filter orders

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -106,7 +106,8 @@ class ContactData extends Component {
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
-      orderData: formData
+      orderData: formData,
+      userId: this.props.userId,
     }
 
     this.props.onOrderBurger(order, this.props.token);
@@ -208,6 +209,7 @@ const mapStateToProps = state => {
     price: state.burgerBuilder.totalPrice,
     loading: state.order.loading,
     token: state.auth.token,
+    userId: state.auth.userId,
   }
 };
 
